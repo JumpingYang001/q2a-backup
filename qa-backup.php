b300046ac7501bb6e5c269ae2a2cc1974a13edaf
Jumping Yang[Wicresoft] <v-zhenpy@microsoft.com>
2016-09-28 16:29:54 +0800

    Update qa-backup.php

--- qa-backup.php
+++ qa-backup.php
@@ -61,8 +61,12 @@ class qa_backup {
 		
 		$this->backupDirUrl = $qa_root_url_relative . BAKFOLDER;
 		$this->backupDir = QA_BASE_DIR.BAKFOLDER;
-		if (!is_dir($this->backupDir))
+		if (!is_dir($this->backupDir)){
 			mkdir($this->backupDir, 0755);
+			if (!is_dir($this->backupDir)){
+				$this->error = 'You do not have permission to create backup folder:'.$this->backupDir.'<br/> Please create and set it manually!';
+			}
+		}    
 
 		if (qa_clicked('backup_send_upload_file')) {
 			$this->saveChanges();
